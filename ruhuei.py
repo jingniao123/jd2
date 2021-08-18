762e20d73f5851591a6b1b3234508936cc8cac70
惊鸟 <68637098+q717772744@users.noreply.github.com>
2021-08-18 10:04:20 +0800

    京东入会开卡领取京豆

--- ruhuei.py
+++ ruhuei.py
@@ -12,6 +12,7 @@ readmes = """
 # JD入会领豆小程序
 ![JD入会领豆小程序](https://raw.githubusercontent.com/curtinlv/JD-Script/main/OpenCrad/resultCount.png)
 
+
 ## 使用方法
 #### [手机用户（参考） https://mp.weixin.qq.com/s/ih6aOURXWM-iKrhvMyR3mw](https://mp.weixin.qq.com/s/ih6aOURXWM-iKrhvMyR3mw)
 #### [PC用户 （参考） https://mp.weixin.qq.com/s/JmLxAecZAlEc4L2sZWnn1A](https://mp.weixin.qq.com/s/JmLxAecZAlEc4L2sZWnn1A)
